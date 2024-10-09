d198dc2f018addb359965ff1dd3295ec9492af91
manu <bmmanu555@gmail.com>
2024-10-09 19:58:48 +0530

    added cors

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const { createTodo, updateTodo } = require("./types");
 const { todo } = require("./db");
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
-
+app.use(cors());
 app.listen(3000);
 
 app.post("/todo", async function(req, res) {
